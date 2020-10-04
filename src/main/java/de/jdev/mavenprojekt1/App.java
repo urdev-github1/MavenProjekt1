9b7504b22062bf9df037514cbb989186c0f6df6d
urdevkoeln <msdeveloper@gmx.net>
2020-10-04 14:54:16 +0200

    1. Snapshot

---
+++ src/main/java/de/jdev/mavenprojekt1/App.java
@@ -0,0 +1,40 @@
+package de.jdev.mavenprojekt1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    //Hauptprogramm
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        //
+        scene = new Scene(loadFXML("primary"));
+
+        //Die Szene der Bühne übergeben.
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("????-Login");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/de/jdev/mavenprojekt1/PrimaryController.java
@@ -0,0 +1,8 @@
+package de.jdev.mavenprojekt1;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+}

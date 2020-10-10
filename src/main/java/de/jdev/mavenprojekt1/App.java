a8ebaa8e76d28779948959d8c1402df59c684aad
urdevkoeln <msdeveloper@gmx.net>
2020-10-10 19:09:34 +0200

    4. Snapshot

--- src/main/java/de/jdev/mavenprojekt1/App.java
+++ src/main/java/de/jdev/mavenprojekt1/App.java
@@ -1,13 +1,19 @@
 package de.jdev.mavenprojekt1;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javax.swing.*;
 import java.io.IOException;
+//
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.PopupMenu;
 
+//
 public class App extends Application {
 
     public static Scene scene;
@@ -29,6 +35,80 @@ public class App extends Application {
 
         //Die Stage anzeigen.
         primaryStage.show();
+
+        //Anwendung nicht beenden, wenn das Passwortfenster geschlossen wird.
+        Platform.setImplicitExit(false);
+
+        // Beenden, wenn die Taskleiste das nicht unterstützt.
+        if (!SystemTray.isSupported()) {
+            System.out.println("Wird von der Taskleiste nicht unterstützt.");
+            Platform.exit();
+        }
+
+        //Bilddatei laden
+        java.awt.Image abasTrayIcon = new ImageIcon(this.getClass().getResource("/images/trayicon.png")).getImage();
+
+        //Items Firmen
+        final java.awt.MenuItem firma1 = new java.awt.MenuItem("Firma1");
+        final java.awt.MenuItem firma2 = new java.awt.MenuItem("Firma2");
+        final java.awt.MenuItem firma3 = new java.awt.MenuItem("Firma3");
+        final java.awt.MenuItem firma4 = new java.awt.MenuItem("Firma4");
+        final java.awt.MenuItem firma5 = new java.awt.MenuItem("Firma5");
+        final java.awt.MenuItem firma6 = new java.awt.MenuItem("Firma6");
+        final java.awt.MenuItem firma7 = new java.awt.MenuItem("Firma7");
+        final java.awt.MenuItem firma8 = new java.awt.MenuItem("Firma8");
+        final java.awt.MenuItem exitItem = new java.awt.MenuItem("Close");
+
+        //Instanz des Popup-Menüs
+        final PopupMenu popup = new PopupMenu();
+
+        //Menüpunkte dem Menü hinzufügen.
+        popup.add(firma8);
+        popup.add(firma7);
+        popup.add(firma6);
+        popup.add(firma5);
+        popup.add(firma4);
+        popup.add(firma3);
+        popup.add(firma2);
+        popup.add(firma1);
+        popup.add(exitItem);
+
+        //Menü dem TrayIcon hinzufügen.
+        TrayIcon trayIcon = new TrayIcon(abasTrayIcon, "MavenProjekt1", popup);
+
+        //Instanz der Klasse SystemTray + TrayIcon in die Taskleiste einfügen.
+        SystemTray tray = SystemTray.getSystemTray();
+
+        try {
+            tray.add(trayIcon);
+        } catch(Exception e) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Exit-Event (Programm beenden).
+        exitItem.addActionListener(event -> System.exit(0));
+
+
+
+
+
+
+
+
     }
 
     //
--- src/main/java/de/jdev/mavenprojekt1/PrimaryController.java
+++ src/main/java/de/jdev/mavenprojekt1/PrimaryController.java
@@ -1,9 +1,38 @@
 package de.jdev.mavenprojekt1;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import java.util.Random;
 
 //Event-Handling
 public class PrimaryController {
 
+    //@FXML - Steht vor private Member oder Methoden und erlaubt den Zugriff aus FXML heraus.
+
+    @FXML
+    private TextField pass2;    //pass2 entspricht der ID in primary.fxml
+
+    @FXML
+    private Button pwgenerator;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+
+        //Passwort erzeugen
+        int length = 8;
+
+        String zeichen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+
+        //Array für 8-Zeichen langes Passwort
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            password[i] = zeichen.charAt(random.nextInt(zeichen.length()));
+        }
+
+        //Passwort in das Textfeld schreiben
+        pass2.setText(String.valueOf(password));
+    }
 }
